8eecec7114163d947e8e1def536976e9e96d411f
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-05-02 11:27:23 +0000

    animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
   static const String id = 'welcome_screen';
 
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+//This is the state object
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+  AnimationController? controller;
+
+  @override 
+  void initState() {
+    super.initState();
+    
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      //The vsync is where we provide the ticker provider and it is most the state object
+      vsync: this,
+      //This is use to reference the state object
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,12 +61,14 @@ class WelcomeScreen extends StatelessWidget {
             Padding(
             padding: const EdgeInsets.all(20.0),
             child: CustomElevatedButton(onPressed: () {
-               Navigator.pushNamed(context, WelcomeScreen.id);
+               Navigator.pushNamed(context, RegistrationScreen.id);
             }, text: "Register", backgroundColor: AppColors.primaryColor,),
             ),
              
           ],
-        )),
+        ),
+        
+        ),
     );
   }
 }
