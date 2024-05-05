b2ffa7d70e56d15667fd14bbcf686292599c2e7a
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-05-05 14:22:32 +0000

    init

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,16 +20,84 @@ class _ChatScreenState extends State<ChatScreen> {
     getCurrentUser();
 
    }
-
+// This method is used to get the current user that is logged in or check if the user is logged in
   void getCurrentUser() async {
-     final user = await _auth.currentUser;
-     if (user != null) {
-     loggedInUser = user;
-     print(loggedInUser!.email);
-     }
+    try {
+      final user = await _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser!.email);
+      }
+    } catch (e) {
+      print(e);
+    }
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              //Implement logout functionality
+              _auth.signOut();
+              Navigator.pop(context);
+            },
+          ),
+        ],
+        title: const Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                ),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        //Do something with the user input.
+                      },
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        hintText: 'Type your message here...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //Implement send functionality.
+                    },
+                    child: const Text(
+                      'Send',
+                      style: TextStyle(
+                        color: Colors.lightBlueAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+            )
+            
+          ],
+        ),
+      ),
+    );
   }
 }
